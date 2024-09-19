28abc27237eb00df7667ee277bd14613091f09c6
Ahmad Fikri Hidayat Al-Islami <fikriihidayat15@gmail.com>
2024-09-19 20:32:55 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,11 @@
+package tipedata;
+
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        int alas = 7, tinggi = 3;
+        
+        double luas = (double) 0.5 * alas * tinggi;
+        System.out.println("Luas Segitiga : " + luas);
+    }
+}
